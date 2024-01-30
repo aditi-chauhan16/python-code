ec098ba545fcd40c1e463c945440a5a279459ffd
aditi-chauhan16 <158132158+aditi-chauhan16@users.noreply.github.com>
2024-01-30 12:09:41 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,20 @@
+'''#prints all letters except 'l' & 'p'
+i=0
+a='welcome
+
+#wap to generate fibonnaci series of a number
+n=int(input("enter the number")
+sum=0'''
+
+"""num = int(input('enter a number: '))
+factorial = 1
+if num < 0:
+  print("factorial doesn't exist for negative numbers")
+elif num == 0:
+  print("factorial of 0 is 1")
+else:
+  for i in range(1, num+1):
+    factorial = factorial * i
+  print("the factorial of the given {num} is " + str(factorial))
+  
+  
