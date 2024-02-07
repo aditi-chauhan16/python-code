3e8b815c5c7646fc86d933522254da49b0c52b5e
aditi-chauhan16 <158132158+aditi-chauhan16@users.noreply.github.com>
2024-02-07 21:11:48 +0530

    Add files via upload

---
+++ specialization.py
@@ -0,0 +1,10 @@
+#WRITE A PROGRAM TO ENTER A USER CHOICE FOR SELECTING SPECIALIZATION IN SOCS 
+specialization=input("enter specialization")    
+if specialization==("AI"):
+    print("SPECIALIZATION IS: AI")
+elif specialization==("CCVT"):
+    print("SPECIALIZATION IS: CCVT")
+elif specialization==("Security"):
+    print("SPECIALIZATION IS: Cyber security")
+else:
+    print("not decided yet")
