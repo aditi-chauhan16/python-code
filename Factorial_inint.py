6134d4dfa5658369f036aa5c127195a1b9bfefbe
aditi-chauhan16 <158132158+aditi-chauhan16@users.noreply.github.com>
2024-03-29 19:06:48 +0530

    Factorial_inint.py

--- Factorial_inint.py
+++ Factorial_inint.py
@@ -1,14 +1,14 @@
+#WRITE A PROGRAM TO GENERATE FACTORIAL OF A NUMBER USING INIT
 class Factorial:
     def __init__(self, n):
         self.n = n
-
+        
     def calculate(self):
         result = 1
         for i in range(1, self.n + 1):
             result *= i
         return result
-
-# Example usage:
+        
 factorial_instance = Factorial(5)
 result = factorial_instance.calculate()
 print(result)  # Output will be 120 (5!)
